49fe996b74ea87b43ae421e5f576c45aa44168b3
M. C. DeMarco <mcd@mcdemarco.net>
2026-06-05 17:53:22 -0400

    Possibly slow mycolor fix

--- src/lib/setRendererColourOpts.js
+++ src/lib/setRendererColourOpts.js
@@ -1,3 +1,4 @@
+import { gameinfo } from "@abstractplay/gameslib";
 export const setRendererColourOpts = ({
   options,
   metaGame,
@@ -23,6 +24,7 @@ export const setRendererColourOpts = ({
     }
     options.coloursGlobal = false;
   }
+
   if (globalMe?.customizations?.[metaGame]) {
     const custom = globalMe.customizations[metaGame];
     if (
@@ -52,16 +54,39 @@ export const setRendererColourOpts = ({
       optioncolours.push(null);
     }
   }
+
   // handle "Always use my colour" preference
   if (
     optioncolours !== undefined &&
     Array.isArray(optioncolours) &&
     optioncolours.length > 0 &&
     globalMe?.settings?.all?.myColor &&
-    isParticipant > 0
+			isParticipant > 0 &&
+			metaGame
   ) {
-    const mycolor = optioncolours.shift();
-    optioncolours.splice(isParticipant, 0, mycolor);
+
+    console.log("b4", optioncolours);
+    const info = gameinfo.get(metaGame);
+    const temp = info?.customizations || [];
+		const customizationHints = temp.filter(h => "player" in h);
+
+    if (customizationHints.length > 0) {
+      //If the game itself has rearranged the palette, need a different swap.
+			const player1Idx = customizationHints.find(h => h.player === 1).num - 1 || 0;
+			const mycolorTemp = optioncolours[player1Idx];
+			console.log(mycolorTemp);
+			const myplayerIdx = customizationHints.find(h => h.player === isParticipant + 1).num - 1;
+			optioncolours[player1Idx] = optioncolours[myplayerIdx];
+			console.log("mid", optioncolours);
+
+			optioncolours[myplayerIdx] = mycolorTemp;
+
+			console.log("after", optioncolours);
+			
+    } else {
+      const mycolor = optioncolours.shift();
+      optioncolours.splice(isParticipant, 0, mycolor);
+    }
   }
   // set option
   if (optioncolours.length > 0) {
